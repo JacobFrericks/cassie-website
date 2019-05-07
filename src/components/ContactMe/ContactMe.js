de4fdf4755e62c6dd13e0e506fbe1fd79c5b0105
Jacob Frericks <jacob.frericks@gmail.com>
2019-05-06 23:01:52 -0500

    Adding correct email api

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -95,8 +95,7 @@ export default class ContactMe extends Component {
     event.preventDefault();
     const self = this;
 
-    // fetch('https://52z0x2c8v9.execute-api.us-east-1.amazonaws.com/default/cassie-website-email', {
-    fetch('https://bejomkze58.execute-api.us-east-1.amazonaws.com/default/email-bobs-vending', {
+    fetch('https://52z0x2c8v9.execute-api.us-east-1.amazonaws.com/default/cassie-website-email', {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
